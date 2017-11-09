2ecf9f9fb3bde9a489ef37c4a3201c60d8952e16
Bradford <1217269+elBradford@users.noreply.github.com>
2017-11-09 18:09:45 -0500

    Improved hop speed benchmark output

--- localizer/tests/benchmark_hop_speed.py
+++ localizer/tests/benchmark_hop_speed.py
@@ -6,6 +6,7 @@ import timeit
 DN = open(os.devnull, 'w')
 
 num_loops = 500
+default_hop_interval = 0.1
 
 iface = get_first_interface()
 channels = range(11)
@@ -18,4 +19,5 @@ def change_channel():
     curr_channel = (curr_channel + 1) % 11
 
 total_time = timeit.timeit(change_channel, number=num_loops)
-print("Average time: {}".format(total_time/num_loops))
+print("Average time: {} ({:.2f}x shorter than default_hop_interval)"
+      .format(total_time/num_loops, default_hop_interval/(total_time/num_loops)))
