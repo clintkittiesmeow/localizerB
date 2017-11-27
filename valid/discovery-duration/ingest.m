48699f743d7dfe7883bf5409307112d304ea6170
Brad Law <1217269+elBradford@users.noreply.github.com>
2017-11-27 17:21:26 -0500

    first attempt at processing discovery results

--- valid/discovery-duration/ingest.m
+++ valid/discovery-duration/ingest.m
@@ -1,7 +1,54 @@
-results = rdir([pwd, '\**\*-results.csv']);
-contents = table();
-for result = results'
-    contents = [contents; importfile(result.name, 2)];
+
+% Get list of directories to gather data from
+files = dir(pwd);
+issub = [files(:).isdir];
+tests = {files(issub).name};
+tests(ismember(tests,{'.','..'})) = [];
+
+% Prepare cell array of tables
+data = cell(length(tests),1);
+
+% Import each test into a table
+for test = 1:length(tests)
+    dirstring = fullfile(pwd, tests(test), '**', '*-results.csv');
+    % Build list of results.csv files to import
+    passes = rdir(char(dirstring));
+    data{test} = table();
+    
+    % Import each results.csv file
+    for pass = passes'
+        data{test} = [data{test}; importfile(pass.name, 2)];
+    end
 end
 
+% List of mac addresses
 macs = splitlines(strtrim(fileread('../../macs')));
+
+% Run through each test and generate metrics
+% Metrics:
+%   Rate = beacons/second
+%   Coverage = % of bssids detected <- large penalty per bssid
+results = cell(length(tests),1);
+for test = 1:length(tests)
+    
+    % Discover how many passes were done for this test
+    passes = unique(data{test}.pass);
+    % Generate an array to store our results in
+    results{test} = zeros(length(passes),2);
+    % Step through each pass of each test
+    for pass = passes'
+        pass_data = data{test}(data{test}.pass == pass,:);
+        results{test}(pass+1,1) = height(pass_data) / data{test}.duration(1);
+        coverage = 0;
+        
+        pass_macs = unique(data{test}.bssid);
+        for mac = macs'
+            if any(pass_macs == mac)
+                coverage = coverage + 1;
+            end
+        end
+        
+        % Convert results to a percentage
+        results{test}(pass+1,2) = coverage / length(macs);
+    end
+end
