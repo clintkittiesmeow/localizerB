06124ab159a3fe1a77e8176c3ee5edb30a18ffec
Bradford <1217269+elBradford@users.noreply.github.com>
2017-11-28 23:15:32 -0500

    UPdate: generate_hop_int improved

--- localizer/utils/generate_hop_int.py
+++ localizer/utils/generate_hop_int.py
@@ -1,11 +1,12 @@
-from math import gcd, ceil
+from math import gcd, ceil, floor
 
 
 TU = 1024/1000000  # 1 TU = 1024 usec https://en.wikipedia.org/wiki/TU_(Time_Unit)
-STD_BEACON_INT = 100*TU
-GENERATOR_SCALE = 10
+STD_BEACON_SCALE = 100
+DEFAULT_START = STD_BEACON_SCALE/10
+DEFAULT_END = STD_BEACON_SCALE*2
 
-def coprime_rate_generator(target=GENERATOR_SCALE, high_scaler=2):
+def coprime_rate_generator(start=DEFAULT_START, end=DEFAULT_END):
     """
     Generate a list of coprime rates that can be used as hop rates that minimize synchronization with standard beacon rate of 100TU
     
@@ -16,11 +17,12 @@ def coprime_rate_generator(target=GENERATOR_SCALE, high_scaler=2):
     :return: List of coprimes
     :rtype: list
     """
+
     _results = {}
-    _limit = int(ceil(target*high_scaler))
-    for i in range(_limit):
-        if gcd(i, target) == 1:
-            _results[i] = round(i*STD_BEACON_INT/GENERATOR_SCALE, 5)
+    # Generate an upper limit
+    for i in range(floor(start), ceil(end)):
+        if gcd(i, STD_BEACON_SCALE) == 1:
+            _results[i] = round(i*TU, 5)
 
     return sorted(_results.items())
 
@@ -30,15 +32,16 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Generate a list of coprimes")
-    parser.add_argument("target",
-                        help="The beacon rate to find alternative rates that are co-prime (no synchronization)",
+    parser.add_argument("start",
+                        help="The start of the list to search for coprimes",
                         nargs='?',
-                        type=int,
-                        default=10)
-    parser.add_argument("scale",
-                        help="Multiplied by the target to set an upper limit",
+                        type=float,
+                        default=DEFAULT_START)
+    parser.add_argument("end",
+                        help="The end of the list to search for coprimes",
                         nargs='?',
-                        default=2)
+                        type=float,
+                        default=DEFAULT_END)
     arguments = parser.parse_args()
 
-    print(coprime_rate_generator(arguments.target, arguments.scale))
+    print(coprime_rate_generator(arguments.start, arguments.end))
